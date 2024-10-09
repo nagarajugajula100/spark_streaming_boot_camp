a6b26b1b4fee460902a57644ba38bda1833b728e
nagarajugajula100 <nagarajugajula100>
2024-10-09 10:43:40 +0000

    added initial steps

---
+++ training_base_notebooks/0-initial/word_count_stream.py
@@ -0,0 +1,90 @@
+# Databricks notebook source
+from pyspark.sql.functions import explode, split, lower,trim
+
+# COMMAND ----------
+
+# Load the Data 
+# /FileStore/tables/boot_camp/datasets/text/
+
+# COMMAND ----------
+
+base_data_dir = "/FileStore/tables/boot_camp"
+
+# COMMAND ----------
+
+# Data Cleanup
+
+# COMMAND ----------
+
+spark.sql("drop table if exists word_count_table")
+dbutils.fs.rm("/user/hive/warehouse/word_count_table", True)
+
+dbutils.fs.rm(f"{base_data_dir}/chekpoint", True)
+dbutils.fs.rm(f"{base_data_dir}/data/text", True)
+
+dbutils.fs.mkdirs(f"{base_data_dir}/data/text")
+
+# COMMAND ----------
+
+itr = 1
+
+# COMMAND ----------
+
+# Load the data to required location
+
+# COMMAND ----------
+
+dbutils.fs.cp(f"{base_data_dir}/datasets/text/text_data_{itr}.txt", f"{base_data_dir}/data/text/")
+
+# COMMAND ----------
+
+lines = (spark.readStream
+                    .format("text")
+                    .option("lineSep", ".")
+                    .load(f"{base_data_dir}/data/text")
+)
+lines_df = lines.select(explode(split(lines.value, " ")).alias("word"))
+
+# COMMAND ----------
+
+# Data Clearning 
+
+# COMMAND ----------
+
+formatted_df = (lines_df.select(lower(trim(lines_df.word)).alias("word"))
+                        .where("word is not null")
+                        .where("word rlike '[a-z]'")
+)
+
+# COMMAND ----------
+
+# Aggregation
+
+# COMMAND ----------
+
+formatted_group_df= formatted_df.groupBy("word").count()
+
+# COMMAND ----------
+
+# Saving teh Data 
+
+# COMMAND ----------
+
+
+ streamQuery = ( formatted_group_df.writeStream
+                    .format("delta")
+                    .option("checkpointLocation", f"{base_data_dir}/chekpoint/word_count")
+                    .outputMode("complete")
+                    .toTable("word_count_table")
+                )
+ # Start the stream and let it run
+streamQuery.awaitTermination()
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from word_count_table;
+
+# COMMAND ----------
+
+streamQuery.stop()
